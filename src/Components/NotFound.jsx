6bb0db6857d4af2e95ed97e023f82ee4699a1cd5
POL0R <dhumalpiyush08@gmail.com>
2024-11-07 12:25:04 +0530

    plase2

--- src/Components/NotFound.jsx
+++ src/Components/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
             <div className="errorCarryingContent">
             <h1 className="errorHead">404</h1>
             <h1 className="errorCarrying">Not Found</h1>
-            <Link to="/home" className="hoverButtonError"><button className="buttonError" >Go Back to Home &#8617;</button></Link>
+            <Link to="/" className="hoverButtonError"><button className="buttonError" >Go Back to Home &#8617;</button></Link>
             </div>
         </div>
     )
